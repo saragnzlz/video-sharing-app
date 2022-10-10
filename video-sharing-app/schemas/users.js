8214fbd30e55d6c2bd39c738ae1d9e2cce1347d1
saragnzlz <sagonzacu@gmail.com>
2022-10-10 20:14:48 +0200

    users config file

---
+++ video-sharing-app/schemas/users.js
@@ -0,0 +1,16 @@
+/* File for managing the user base */
+
+export default{
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'userName',
+            title: 'Username',
+            type: 'String',
+        },
+        {
+            name: 'image',
+        }
+    ]
+}
